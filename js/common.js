b629b3af0f5a926fa80016f61f6f907f70ebeeeb
KapishDima <kapishdima@gmail.com>
2019-05-02 21:09:29 +0300

    Сверстана и адаптирована секция Comments

--- js/common.js
+++ js/common.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
+    // Burger Button Animation
     $('.navbar__block-menu-burger-link').on('click', function(e) {
         e.preventDefault;
         $(this).toggleClass('navbar__block-menu-burger-link-active');
     });
 
+    // EqualHeights
     $('.services__block_service-wrapper-item-img').equalHeights();
+
+
+    //OwlCarousel
+    $('.comment__block-wrapper-slider').slick({
+        adaptiveHeight: true,
+        centerMode: true,
+        dots: true,
+        fade: true,
+        responsive: [
+            {
+            breakpoint: 830,
+            settings: {
+                dots: false
+            }
+        }
+    ]
+    });
 });
